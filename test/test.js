1e7edc51f65732f6f77f8d07268f75cc42d8c6e1
kira0x1 <kira.eva@protonmail.com>
2021-03-04 02:04:08 +0300

    added youtube url and id tests
    - also renamed regexResult to regExResult

--- test/test.js
+++ test/test.js
@@ -1,16 +1,28 @@
 const assert = require('assert')
+const { validateURL, validateID } = require('ytdl-core')
 
 const spotiyRegExp = new RegExp(/^(spotify:|https:\/\/[a-z]+\.spotify\.com\/)/)
 
-describe('Spotify', () => {
+describe('Music', () => {
     describe('Url', () => {
         it('should return true since the link is a valid spotify link', () => {
-            assert.equal(spotiyRegExp.test('https://open.spotify.com/track/7EwNGyM05261K8rxXvtSg4?si=ox39iGdVRJWXGKiybaNCeQ'), true)
+            const regExResult = spotiyRegExp.test('https://open.spotify.com/track/7EwNGyM05261K8rxXvtSg4?si=ox39iGdVRJWXGKiybaNCeQ')
+            assert.strictEqual(regExResult, true)
         })
 
         it('should return false since this is not a valid spotify link', () => {
-            const regexResult = spotiyRegExp.test('https://youtu.be/yjrdJBP5i1I')
-            assert.equal(regexResult, false)
+            const regExResult = spotiyRegExp.test('https://youtu.be/yjrdJBP5i1I')
+            assert.strictEqual(regExResult, false)
+        })
+
+        it('should return true when given a valid youtube url', () => {
+            const isYt = validateURL('https://youtu.be/yjrdJBP5i1I')
+            assert.strictEqual(isYt, true)
+        })
+
+        it('should return true if a valid youtube id is given', () => {
+            const isYt = validateID('yjrdJBP5i1I')
+            assert.strictEqual(isYt, true)
         })
     })
 })
